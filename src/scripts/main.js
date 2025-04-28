d0f2013c6226dd8da35310ad99ec6418e190fb5e
petrushinsky-kirill <petrushinsky@gmail.com>
2025-04-28 16:24:48 +0300

    solution upd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const table = document.querySelector('table');
-const tableHead = table.querySelector('thead tr');
-const tableBody = table.querySelectorAll('tbody tr');
-const tableFoot = table.querySelector('tfoot tr');
+const tableHeadRow = table.querySelector('thead tr');
+const tableBodyRow = table.querySelectorAll('tbody tr');
+const tableFootRow = table.querySelector('tfoot tr');
 
-const newHeadRow = tableHead.children[1].cloneNode(true);
-const newFootRow = tableFoot.children[1].cloneNode(true);
+const newHeadRow = tableHeadRow.children[1].cloneNode(true);
+const newFootRow = tableFootRow.children[1].cloneNode(true);
 
-tableHead.lastElementChild.before(newHeadRow);
-tableFoot.lastElementChild.before(newFootRow);
+tableHeadRow.lastElementChild.before(newHeadRow);
+tableFootRow.lastElementChild.before(newFootRow);
 
-for (const row of tableBody) {
+for (const row of tableBodyRow) {
   const newbodyRow = row.children[1].cloneNode(true);
 
   row.lastElementChild.before(newbodyRow);
