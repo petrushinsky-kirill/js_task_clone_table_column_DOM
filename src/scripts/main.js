a3c735558a66f9956aa27b063f75a9ae61c96740
petrushinsky-kirill <petrushinsky@gmail.com>
2025-04-28 16:35:41 +0300

    solution upd2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,28 @@
 'use strict';
 
-const table = document.querySelector('table');
-const tableHeadRow = table.querySelector('thead tr');
-const tableBodyRow = table.querySelectorAll('tbody tr');
-const tableFootRow = table.querySelector('tfoot tr');
+const trList = [...document.querySelectorAll('tr')];
 
-const newHeadRow = tableHeadRow.children[1].cloneNode(true);
-const newFootRow = tableFootRow.children[1].cloneNode(true);
+trList.forEach((tr) => {
+  if (tr.cells[1] && tr.cells[tr.children.length - 2]) {
+    const copy = tr.cells[1].cloneNode(true);
 
-tableHeadRow.lastElementChild.before(newHeadRow);
-tableFootRow.lastElementChild.before(newFootRow);
+    tr.cells[tr.children.length - 2].insertAdjacentElement('afterend', copy);
+  }
+});
 
-for (const row of tableBodyRow) {
-  const newbodyRow = row.children[1].cloneNode(true);
+// const table = document.querySelector('table');
+// const tableHeadRow = table.querySelector('thead tr');
+// const tableBodyRow = table.querySelectorAll('tbody tr');
+// const tableFootRow = table.querySelector('tfoot tr');
 
-  row.lastElementChild.before(newbodyRow);
-}
+// const newHeadRow = tableHeadRow.children[1].cloneNode(true);
+// const newFootRow = tableFootRow.children[1].cloneNode(true);
+
+// tableHeadRow.lastElementChild.before(newHeadRow);
+// tableFootRow.lastElementChild.before(newFootRow);
+
+// for (const row of tableBodyRow) {
+//   const newbodyRow = row.children[1].cloneNode(true);
+
+//   row.lastElementChild.before(newbodyRow);
+// }
